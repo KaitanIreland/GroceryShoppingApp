543bba3f8e3709b2be6b1be19058cb3594c9fcbf
KaitanKale <kaitan.ireland98@gmail.com>
2018-04-04 18:24:46 -0500

    What is the total price of items purchased on date [D]?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,14 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
+        System.out.println("Hello! Welcome to the Grocery Shopping App.");
+        System.out.println("===========================================");
         ArrayList<LocalDate> localDates = new ArrayList<>();
         ArrayList<String> products = new ArrayList<>();
         ArrayList<Integer> prices = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        LocalDate today = LocalDate.of(2018,4,4);
+        LocalDate today = LocalDate.of(2018, 4, 4);
         localDates.add(today.minusDays(94));
         localDates.add(today.minusDays(3));
         localDates.add(today.minusDays(64));
@@ -28,18 +29,25 @@ public class Main {
         prices.add(9);
         prices.add(8);
 
-	//How many items were purchased during the last visit to the grocery store?
+        //How many items were purchased during the last visit to the grocery store?
         LocalDate agesAgo = today.minusYears(30);
         LocalDate minimum;
         String purchasedLast = " ";
+        int costOfTrip = 0;
         for (int i = 0; i < localDates.size(); i++) {
             if (localDates.get(i).isAfter(agesAgo)) {
                 minimum = localDates.get(i);
                 purchasedLast = products.get(i);
+                costOfTrip=prices.get(i);
             }
         }
         System.out.println("During the last visit to the grocery store, these items were purchased:" + "\n" + purchasedLast);
-    //TODO
-    //TODO How
+
+        //What is the total price of items purchased on date [D]
+        //I'm assuming date D is the most recent date
+        System.out.println("\nThe total cost of all of these items was:" + "\n" + "$" + costOfTrip);
+
+
+        //TODO How
     }
 }
