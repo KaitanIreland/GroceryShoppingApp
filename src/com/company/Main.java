92829cd6276f1218a8f1bc1fcda6c5092c49a296
KaitanKale <kaitan.ireland98@gmail.com>
2018-04-05 11:58:41 -0500

    Pushed code with functions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,28 @@ public class Main {
         ArrayList<LocalDate> localDates = new ArrayList<>();
         ArrayList<String> products = new ArrayList<>();
         ArrayList<Integer> prices = new ArrayList<>();
+        initializeArrayLists(localDates, products, prices);
+
+        //How many items were purchased during the last visit to the grocery store?
+        System.out.println(howManyItems(localDates, products, prices));
+
+
+        //What is the total price of items purchased on date [D]
+        //I'm assuming date D is the most recent date
+
+        System.out.println("\nThe total cost of all of these item(s) was:" + "\n" + "$" + findCostOfTrip(localDates,products,prices));
+
+
+        //How many times was [A] purchased?
+        System.out.println(findTimesPurchased(localDates,products,prices));
+
+
+
+        //TODO What items were purchased that cost more than $10?
+
+
+    }
+    private static void initializeArrayLists(ArrayList<LocalDate> localDates, ArrayList<String> products, ArrayList<Integer> prices) {
         Scanner sc = new Scanner(System.in);
         LocalDate today = LocalDate.of(2018, 4, 4);
         localDates.add(today.minusDays(94));
@@ -29,7 +51,9 @@ public class Main {
         prices.add(9);
         prices.add(8);
 
-        //How many items were purchased during the last visit to the grocery store?
+    }
+    private static String howManyItems(ArrayList<LocalDate> localDates, ArrayList<String> products, ArrayList<Integer> prices) {
+        LocalDate today = LocalDate.of(2018, 4, 4);
         LocalDate agesAgo = today.minusYears(30);
         LocalDate minimum;
         String purchasedLast = " ";
@@ -38,26 +62,48 @@ public class Main {
             if (localDates.get(i).isAfter(agesAgo)) {
                 minimum = localDates.get(i);
                 purchasedLast = products.get(i);
-                costOfTrip=prices.get(i);
+                costOfTrip = prices.get(i);
             }
         }
-        System.out.println("During the last visit to the grocery store, these item(s) were purchased:" + "\n" + purchasedLast);
-
-        //What is the total price of items purchased on date [D]
-        //I'm assuming date D is the most recent date
-        System.out.println("\nThe total cost of all of these item(s) was:" + "\n" + "$" + costOfTrip);
-
-
-        //How many times was [A] purchased?
+        String statement = "During the last visit to the grocery store, these item(s) were purchased:" + "\n" + purchasedLast;
+        return statement;
+    }
+    private static Integer findCostOfTrip(ArrayList<LocalDate> localDates, ArrayList<String> products, ArrayList<Integer> prices) {
+        LocalDate today = LocalDate.of(2018, 4, 4);
+        LocalDate agesAgo = today.minusYears(30);
+        LocalDate minimum;
+        String purchasedLast = " ";
+        int costOfTrip = 0;
+        for (int i = 0; i < localDates.size(); i++) {
+            if (localDates.get(i).isAfter(agesAgo)) {
+                minimum = localDates.get(i);
+                purchasedLast = products.get(i);
+                costOfTrip = prices.get(i);
+            }
+        }
+        return costOfTrip;
+    }
+    private static String findTimesPurchased(ArrayList<LocalDate> localDates, ArrayList<String> products, ArrayList<Integer> prices) {
+        LocalDate today = LocalDate.of(2018, 4, 4);
+        LocalDate agesAgo = today.minusYears(30);
+        String purchasedLast = " ";
+        int costOfTrip = 0;
         int count = 0;
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).contains(purchasedLast)) {
-                count = count + 1;
+        int sum;
+        LocalDate maxDate = localDates.get(0);
+        for (int u = 0; u < localDates.size(); u++) {
+            if (localDates.get(u).isAfter(maxDate)) {
+                maxDate = localDates.get(u);
             }
         }
-        System.out.println("\n" + purchasedLast + " was purchased " + count + " time(s).");
-
-        //TODO What items were purchased that cost more than $10?
-
+        String productCorrelated = " ";
+        for (int i = 0; i < localDates.size(); i++) {
+            if (localDates.get(i).equals(maxDate)) {
+                productCorrelated = products.get(i);
+                    }
+                }
+        String statement2 = "\n" + productCorrelated + " was purchased " + count + " time(s).";
+        return statement2;
     }
 }
+
